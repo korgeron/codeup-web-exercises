e064c107c427b52adcdfd1cf3d37519ab0ead3bb
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-13 16:05:23 -0500

    practice graded exercise(still working on)

---
+++ js/external.js
@@ -0,0 +1,25 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+//JS with HTML Exercises
+/*
+// problem 1
+alert('Welcome to my Website!');
+// problem 2
+let color = prompt("What is your favorite color?");
+
+alert(color + " is my favorite color too.");
+
+ */
+
+// problem 3
+let question = confirm("Would you like to rent a movie?");
+let choices = ("Your choices are: The little Mermaid, Brother Bear, Hercules");
+    if (question >= 1) {
+        confirm(choices);
+    }
+
+
+
+
+
