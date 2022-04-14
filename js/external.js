78c99eeec042704af7e7ce6903daf2871db6be0b
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-14 16:46:57 -0500

    cleaning up code / testing for external.js

--- js/external.js
+++ js/external.js
@@ -10,82 +10,70 @@ let color = prompt("What is your favorite color?");
 
 alert(color + " is my favorite color too.");
 
+
+
  */
 
-/* PROBLEM 3
-let movie1 = "The little Mermaid";
-let movie2 = "Brother Bear";
-let movie3 = "Hercules";
-
-let choice1 = "Would you like to rent " + movie1;
-let choice2 = "Would you like to rent " + movie2;
-let choice3 = "Would you like to rent " + movie3;
-
-let customerChoice = choice1 || choice2 || choice3;
-let question = confirm("Would you like to rent a movie for $3 per day?");
-let choices = ("Your Choices Are: " + movie1 + ", " + movie2 + ", " + movie3);
-
-let days1 = "How many days would you like?";
-let days2 = "How many days would you like?";
-let days3 = "How many days would you like?";
-let chosenDays = prompt(days).indexOf();
-*/
+//PROBLEM 3 STILL WORKING ON
 let question = confirm("Would you like to rent a movie for $3 per day?");
     if (question >= 1) {
-
     } else {
        onclose(alert("Maybe next time."))
-
     }
 let movie1 = "The little Mermaid";
 let movie2 = "Brother Bear";
 let movie3 = "Hercules";
 let choices = ("Your Choices Are: " + movie1 + ", " + movie2 + ", " + movie3);
     if (confirm(choices) >= 1) {
-
   } else {
         onclose(alert("Bad selections? OOPSE!!"))
     }
-function total1(number) {
-    return console.log(parseInt(prompt(days1)) * 3);
+function total1() {
+    let totalDays1 = parseInt(prompt("Enter number of days to rent: "))
+    return (totalDays1 * 3)
 }
 let choice1 = "Would you like to rent " + movie1;
-let days1 = "How many days would you like?";
+/*function total1() {
+  let totalDays1 = parseInt(prompt("Enter number of days to rent: "))
+    return (totalDays1 * 3)
+}
+ */
     if (confirm(choice1) >= 1) {
-        console.log(parseInt(prompt(days1)) * 3);
+        total1()
     } else {
         total1 = 0;
     }
-function total2(number) {
-    return console.log(prompt(days2) * 3);
+function total2() {
+    let totalDays2 = parseInt(prompt("Enter number of days to rent: "))
+    return (totalDays2 * 3)
 }
 let choice2 = "Would you like to rent " + movie2;
-let days2 = "How many days would you like?";
+/*function total2() {
+    let totalDays2 = parseInt(prompt("Enter number of days to rent: "))
+    return (totalDays2 * 3)
+}
+ */
     if (confirm(choice2) >= 1){
-        console.log(prompt(days2));
+        total2()
     } else {
         total2 = 0;
     }
-function total3(number) {
-    return
-    console.info(days3)
+function total3() {
+    let totalDays3 = parseInt(prompt("Enter number of days to rent: "))
+    return (totalDays3 * 3)
 }
 let choice3 = "Would you like to rent " + movie3;
-let days3 = "How many days would you like?";
+/*function total3() {
+    let totalDays3 = parseInt(prompt("Enter number of days to rent: "))
+    return (totalDays3 * 3)
+}
+ */
     if (confirm(choice3) >= 1) {
-        console.log(prompt(days3));
+        total3()
     } else {
         total3 = 0;
     }
     alert(total1() + total2() + total3());
-    /*function total1(number) {
-      return console.log(prompt(days1) * 3);
-  }
-    function total2(number) {
-       return console.log(prompt(days2) * 3);
-   }
-    function total3(number) {
-    return console.log(prompt(days3) * 3);
-}
-*/
+
+
 
