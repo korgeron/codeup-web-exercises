bf7c10194a7eb6153603f5ed639ce358936d27dc
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-16 16:52:39 -0500

    finished problem for 3.1

--- my-stuff/external-me.js
+++ my-stuff/external-me.js
@@ -26,7 +26,7 @@ function movies () {
   let movie3 = "Hercules";
 //SECOND POP-UP WINDOW
 function choices () {
-    return confirm("Your choices are: " + movie1 +", "+ movie2 +", "+"or "+ movie3 + ".") === true;
+    return confirm("Your choices are: " + movie1 +", "+ movie2 +", "+"or "+ movie3 + ".")
 }
 
 //CALLS ^ FUNCTION / PASSES TO NEXT
@@ -37,39 +37,57 @@ function choices () {
 
 //FIRST MOVIE CHOICE
 function choice1 () {
- return confirm("Would you like to rent " + movie1 + "?")
+    return confirm("Would you like to rent " + movie1 + "?")
 }
-    if (choice1().valueOf())
-    {
-        let days1 = prompt("Enter number of days you would like to rent for:") === true;
-
+let total1;
+    if (choice1().valueOf()) {
+        let days1 = prompt("Enter number of days you would like to rent for:")
+        alert("You have chosen: " + (days1) + " day/s.")
+           alert("The cost for this movie is: " + "$" + (days1 * 3))
+                total1 = days1 * 3
     } else {
         let days1 = 0
+        alert("You have chosen: " + (days1) + "day/s.")
+            total1 = days1
     }
+
+
 
 //SECOND MOVIE CHOICE
 function choice2 () {
     return confirm("Would you like to rent " + movie2 + "?")
 }
-    if (choice2().valueOf())
-    {
-        let days2 = prompt("Enter number of days you would like to rent for:") === true;
-
+let total2;
+    if (choice2().valueOf()) {
+        let days2 = prompt("Enter number of days you would like to rent for:")
+        alert("You have chosen: " + (days2) + " day/s.")
+           alert("The cost for this movie is: " + "$" + (days2 * 3))
+                total2 = days2 * 3
     } else {
         let days2 = 0
+        alert("You have chosen: " + (days2) + "day/s.")
+            total2 = days2
     }
+
+
 
 //THIRD MOVIE CHOICE
 function choice3 () {
     return confirm("Would you like to rent " + movie3 + "?")
 }
+let total3;
     if (choice3().valueOf()) {
-        let days3 = prompt("Enter number of days you would like to rent for:") === true;
-
+        let days3 = prompt("Enter number of days you would like to rent for:")
+        alert("You have chosen: " + (days3) + " day/s.")
+           alert("The cost for this movie is: " + "$" + (days3 * 3))
+                total3 = days3 * 3
     } else {
         let days3 = 0
+        alert("You have chosen: " + (days3) + "day/s.")
+            total3 = days3
     }
 
 
+alert("Your total is " + "$" +(total1 + total2 + total3) + ".");
 
 } ())
