4b57cc63b5b78dd47d5ae1c96d34c5707e0dd53b
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-19 23:26:27 -0500

    messing around

---
+++ my-stuff/parse-it.js
@@ -0,0 +1,7 @@
+"use strict";
+(function () {
+
+let parseThis = "Try and get out this  56 located in this sentence!"
+    let parseProcess = parseThis.indexOf("number");
+    console.log(parseFloat(parseProcess));
+} ())
