d23f402a60c941ebc106ee563254150cab9bdaa1
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-21 16:59:00 -0500

    working on exercise

---
+++ js/for_loops.js
@@ -0,0 +1,69 @@
+"use strict";
+
+//PROBLEM 1
+/**TODO:
+ * create for loop
+ * takes in a number
+ * console.log the times-table for that number input
+ * times table goes from 1-10
+ * ex. 7 x 1 = 7
+ *     7 x 10 = 70
+ **/
+// function showMultiplicationTable(num){
+//     for (let multiplyBy = 1;multiplyBy < 11;multiplyBy++){
+//        let table = num * multiplyBy;
+//        let answer = num +" x " + multiplyBy + " = " + table
+//             console.log(answer);
+//    }
+// }
+// showMultiplicationTable(7);
+
+//PROBLEM 2
+/**TODO:
+* create for loop
+ * generate 10 random numbers (between 20 and 200
+ * output to the console whether each number is odd or even
+ **/
+// function evenOrOdd() {
+//     for (let cycleCount = 1; cycleCount < 11; cycleCount++) {
+//         let numberGenerator = Math.floor(Math.random() * 181) + 20;
+//
+//         if (numberGenerator % 2 === 0) {
+//             let isEven = (numberGenerator + " is even ")
+//             console.log(isEven);
+//             continue;
+//         }
+//             let isOdd = (numberGenerator + " is odd ")
+//             console.log(isOdd);
+//         }
+//
+// }
+// evenOrOdd();
+
+//PROBLEM 3
+/**TODO:
+ * create for loop
+ * uses console.log
+ * displays:
+ * 1
+ * 22
+ * 333
+ * 4444
+ * 55555
+ * 666666
+ * 7777777
+ * 88888888
+ * 999999999
+ **/
+
+function oneMoreNumber(){
+let num2 = "2"
+    for (let num = "1"; num < 10; num++){
+        console.log(num);
+    }
+}
+oneMoreNumber();
+
+
+
+
