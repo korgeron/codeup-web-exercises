a9b54daf58218fc428ff0428c45d7f6d421b8c8c
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-22 09:28:27 -0500

    warm-up

--- js/warmups.js
+++ js/warmups.js
@@ -1,1 +1,24 @@
-console.log("Yay! I completed the warm-up!")
+console.log("Yay! I completed the warm-up!");
+
+//FizzBuzz
+/**TODO:
+ * fizzBuzz
+ * log "fizz" if number is evenly divided by 3
+ * log "buzz" if number is evenly divided by 5
+ * log "FizzBuzz" if evenly divided by both
+ * log the number if not evenly divided by either
+ **/
+
+function fizzBuzz(x){
+
+    if ((x / 5) % 2 === 0){
+        console.log("Buzz");
+    } else if ((x / 3) % 2 === 0){
+        console.log("Fizz");
+    }// else if ((x / 3) % 2 === 0 && ((x / 5) % 2 === 0)){
+       // console.log("FizzBuzz");
+    //}
+       // console.log(x);
+
+}
+fizzBuzz(5);
