21ab9cb6e71b5e32f429cf2c14a86c7db53718b6
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-25 15:14:16 -0500

    look exercise finished

--- js/while.js
+++ js/while.js
@@ -10,3 +10,23 @@ let multiplesOfTwo = function (){
 }
 multiplesOfTwo();
 
+//PROBLEM 2 (DO WHILE LOOP)
+
+function iceCreamTruckProgram () {
+
+    let allCones = Math.floor(Math.random() * 51) + 50;
+        do {
+            let iceCreamOrder = Math.floor(Math.random() * 5) + 1;
+                console.log("Customer orders " + iceCreamOrder + " cone(s). We have " + allCones + " left in stock.");
+            if (iceCreamOrder > allCones) {
+            console.log("Cant do this order...Sorry!");
+            } else {
+                allCones -= iceCreamOrder;
+                console.log("We have " + allCones + " cones left in stock.");
+        }
+    } while (allCones > 0);
+
+    console.log("Man! That was rough... Sold out of all my cones.");
+}
+
+iceCreamTruckProgram();
