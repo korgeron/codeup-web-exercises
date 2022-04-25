188c0be7cd346d9884693fcafc27610f8167481e
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-24 20:52:07 -0500

    practing loops

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,19 +1,19 @@
 "use strict";
 /**TODO:
- *
+ * prompt user for odd number between 1 and 50
+ * use a loop and break statement to continue prompting user if invalid input
 **/
-
-//TODO: WALK THROUGH
-// let userNum;
-// while (true){
-//     userNum = parseFloat(prompt("Give odd number between 1 and 50."))    ;
-//         if (userNum % 2!==0 && userNum >= 1 && userNum <= 50){
-//             break;
-//         }
-// }    for (let j = 1; j <=50;j+=2){
-//         if (j===userNum){
-//             console.log("Whoop! Skipping " +userNum);
-//             continue;
-//         }
-//         console.log(j);
-//     }
+let questionAsked;
+    while(true){
+        questionAsked = prompt("Odd number between 1 and 50");
+            if (questionAsked % 2 !== 0 && questionAsked >= 1 && questionAsked <= 50){
+                break;
+            }
+    for (let i = 1; i <= 50; i+=2){
+            if (i===questionAsked){
+                console.log("WHOOP! Skipping " + questionAsked);
+                continue;
+            }
+        console.log(i);
+    }
+}
