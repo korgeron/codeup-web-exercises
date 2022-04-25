ffee3effa80d45aa60231318cb6f496f99ace77f
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-25 12:20:00 -0500

    objects classwork

--- js/planets-string.js
+++ js/planets-string.js
@@ -23,6 +23,16 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    // GOT IT RIGHT BUT DID THE LONG WAY
 let planetBr = "Mercury<br>Venus<br/>Earth<br/>Mars<br/>Jupiter<br/>Saturn<br/>Uranus<br/>Neptune";
     console.log(planetBr);
+
+//TODO: WALK THROUGH
+// let planetWalkThrough = planetsArray.join("<br>");
+
+//TODO: BONUS
+//     document.body.innerHTML += planetBr;
+//     let planetsUl = "<ul><li>" + planetsArray.join("</li><li>") + "</li></ul>";
+//     document.body.innerHTML += planetsUl;
 } ())
