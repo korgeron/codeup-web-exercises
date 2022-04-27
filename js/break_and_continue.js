4869d659f4c11423591226861a084a59b39721e2
orgeron.kevin <orgeron.kevin@gmail.com>
2022-04-26 19:07:42 -0500

    still working on

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,17 +3,20 @@
  * prompt user for odd number between 1 and 50
  * use a loop and break statement to continue prompting user if invalid input
 **/
-let questionAsked;
-    while(true){
-        questionAsked = prompt("Odd number between 1 and 50");
-            if (questionAsked % 2 !== 0 && questionAsked >= 1 && questionAsked <= 50){
-                break;
-            }
-    for (let i = 1; i <= 50; i+=2){
-            if (i===questionAsked){
-                console.log("WHOOP! Skipping " + questionAsked);
-                continue;
-            }
-        console.log(i);
+function oddNumbers() {
+    let i;
+
+         while (true) {
+             i = prompt('Give me an odd number between 1 and 50');
+             if (i % 2 !== 0 && i <= 50 && i >= 1) {
+                 break;
+        }
+    }           for (let x = 1; x <=50; x+=2) {
+                    if (x === i){
+                        console.log("oopse skipped " + i);
+                        continue;
+                    }
+                             console.log(x);
     }
 }
+oddNumbers();
