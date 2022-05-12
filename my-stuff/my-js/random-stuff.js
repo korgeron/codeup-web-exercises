3ce6de43aab397475d2c76e4c4d6a64c52b5938d
orgeron.kevin <orgeron.kevin@gmail.com>
2022-05-12 16:58:19 -0500

    studying

---
+++ my-stuff/my-js/random-stuff.js
@@ -0,0 +1,51 @@
+//TODO:
+// make function to take in array and return
+// the array in alphabetical order
+
+// let thisArray = ['Hello World nfhs hsfhs hsfsd'];
+//
+// console.log(thisArray);
+//
+// function isAlphabetical(arr) {
+//     let bucket = [];
+//     bucket.push(arr.toString().toLowerCase().split('').sort().join('').trim());
+//     return bucket;
+// }
+//
+// console.log(isAlphabetical(thisArray));
+
+
+//TODO:
+// Write a function, getOlder() that takes in array of dog objects
+// and increases the value of each object's age property by 1.
+
+// let dogObject = [
+//     {
+//         name: 'bluey',
+//         breed: 'lab',
+//         age: 1
+//     },
+//
+//     {
+//         name: 'momo',
+//         breed: 'pug',
+//         age: 5
+//     },
+//
+//     {
+//         name: 'boomboom',
+//         breed: 'pit-bull',
+//         age: 20
+//     }
+//
+// ];
+// function getsOlder(arr){
+//     for (let elem of arr){
+//         let theirAge = elem.age;
+//         let newAge = theirAge + 1;
+//    let result = elem.name + ' the ' + elem.breed + ' was ' + theirAge + ' years old, now they are ' + newAge + ' years old.'
+//         console.log(result);
+//     }
+//
+// }
+// getsOlder(dogObject);
