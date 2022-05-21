be2a75fe67e7dbbebbec8be91a025e5faabc812f
orgeron.kevin <orgeron.kevin@gmail.com>
2022-05-20 23:32:05 -0500

    working on timer for bucket reset

--- js/konami.js
+++ js/konami.js
@@ -1,17 +1,33 @@
 $(function () {
     "use strict";
-    // $('#my-alert').css('display','none');
+    $('.text-alert').css('color', 'rgb(0,0,0,0)');
     let bucket = [];
     $(document).keyup(function (event) {
         bucket.push(event.key);
         let str = bucket.toString();
         console.log(str);
         if (str === 'ArrowUp,ArrowUp,ArrowDown,ArrowDown,ArrowLeft,ArrowRight,ArrowLeft,ArrowRight,a,b') {
-            $('body').css('background','lightgreen');
+            $('body').css('background', 'lightgreen');
             $('#lives').html('LIFE: 30');
-            // $('#my-alert').css('display','block');
+            $('#my-alert').css('color', 'black');
             $('#my-audio')[0].play();
             bucket = [];
+            if ($('#my-alert').css('color', 'red')) {
+                setTimeout(function () {
+                    $('.text-alert').css('color', 'rgb(0,0,0,0)');
+                }, 1500)
+            }
         }
+
+        setTimeout(function () {
+            if (str !== 'ArrowUp,ArrowUp,ArrowDown,ArrowDown,ArrowLeft,ArrowRight,ArrowLeft,ArrowRight,a,b') {
+                return bucket = [];
+            }
+        }, 5000);
+
+
+
+
+        console.log(bucket);
     });
 });
