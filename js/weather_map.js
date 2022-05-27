81c9918a1d1b1436b587da4fd6f438d0bb3f151f
orgeron.kevin <orgeron.kevin@gmail.com>
2022-05-27 18:12:17 -0500

    functionality for search-bar/map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,24 +1,51 @@
-const URL = 'https://api.openweathermap.org/data/3.0/onecall?'
-$.ajax(URL ,{
-    method: 'GET',
-    lat: 98.4946,
-    lon: 29.4252,
-    units: 'imperial',
-    appid: OPEN_WEATHER_KEY
-}).done(function (data){
-    console.log(data);
-});
-
-
-
+const URL = "https://api.openweathermap.org/data/2.5/onecall";
 mapboxgl.accessToken = MAPBOX_API_KEY;
+
 const MAP = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: [-98.4946, 29.4252], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
-$('#map-searchIcon-ID').click(function (){
-    alert('works');
-})
+
+let click;
+let testing;
+$('#map-button-ID').click(function () {
+    //FUNCTIONALITY FOR REMOVING A MARKER FOR A NEW ONE
+    let searchAddress = $('#map-search-ID').val();
+    if (click > 0 && searchAddress) {
+        testing.remove()
+    }
+
+    geocode(searchAddress, MAPBOX_API_KEY).then(function (coordinates) {
+        //FUNCTIONALITY FOR INCREASING CLICK / ADDITION TO REMOVING MARKER FUNCTIONALITY
+        click = 0;
+        click++
+
+        $.get(URL, {
+            appid: OPEN_WEATHER_KEY,
+            lat: coordinates[1],
+            lon: coordinates[0],
+            unit: 'imperial'
+        }).done(function (weather) {
+            console.log(weather);
+        })
+
+        const MARKER = new mapboxgl.Marker()
+            .setLngLat([coordinates[0], coordinates[1]])
+            .addTo(MAP);
+        testing = MARKER;
+        //ADDS FLY TO FUNCTIONALITY
+        MAP.flyTo({
+            center: [coordinates[0], coordinates[1]],
+            zoom: 10,
+            speed: 1,
+            curve: 1
+        })
+        console.log(click);
+
+    })
+});
+
+
