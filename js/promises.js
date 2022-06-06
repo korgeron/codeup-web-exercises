ab860569eb64bcacc784195db9747eb59b74902a
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-06 12:07:30 -0500

    code cleanup

--- js/promises.js
+++ js/promises.js
@@ -3,9 +3,12 @@ const URL = `https://api.github.com/users/${userName}/events/public`;
 // const URL = `https://api.github.com/users/korgeron/events/public`;
 
 
-fetch(URL, {headers: {'Authorization': `token ${GITHUB_TOKEN_KEY}`}}).then((resp) => {
-    return resp.json();
-})
+fetch(URL, {headers: {'Authorization': `token ${GITHUB_TOKEN_KEY}`}})
+
+
+    .then((resp) => {
+        return resp.json();
+    })
 
 
     .then((data) => {
@@ -16,10 +19,10 @@ fetch(URL, {headers: {'Authorization': `token ${GITHUB_TOKEN_KEY}`}}).then((resp
         });
     })
 
-    .then((filter) => {
-        console.log(filter);
-        console.log(filter[0].created_at);
-        return filter[0].created_at;
+    .then((results) => {
+        // console.log(filter);
+        console.log(results[0].created_at);
+        return results[0].created_at;
     })
 
     .catch((error) => {
