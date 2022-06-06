f1ffd3cda6600a19b20ecb9acafb96ba65e7c62d
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-06 13:55:47 -0500

    fun

--- js/promises.js
+++ js/promises.js
@@ -1,30 +1,38 @@
-let userName = prompt('enter username');
-const URL = `https://api.github.com/users/${userName}/events/public`;
+// let userName = prompt('enter username');
+// const URL = `https://api.github.com/users/${userName}/events/public`;
 // const URL = `https://api.github.com/users/korgeron/events/public`;
 
 
-fetch(URL, {headers: {'Authorization': `token ${GITHUB_TOKEN_KEY}`}})
 
 
-    .then((resp) => {
-        return resp.json();
-    })
+const getlastCommitEvent = (username) => {
+    const URL = `https://api.github.com/users/${username}/events/public`;
+    fetch(URL, {headers: {'Authorization': `token ${GITHUB_TOKEN_KEY}`}})
 
 
-    .then((data) => {
-        return data.filter((filteredData) => {
-            if (filteredData.type === 'PushEvent') {
-                return filteredData
-            }
-        });
-    })
+        .then((resp) => {
+            return resp.json();
+        })
 
-    .then((results) => {
-        // console.log(filter);
-        console.log(results[0].created_at);
-        return results[0].created_at;
-    })
 
-    .catch((error) => {
-        console.error(error);
-    })
+        .then((data) => {
+            return data.filter((filteredData) => {
+                if (filteredData.type === 'PushEvent') {
+                    return filteredData
+                }
+            });
+        })
+
+        .then((results) => {
+            // console.log(filter);
+            console.log(results[0].created_at);
+            return results[0].created_at;
+        })
+
+        .catch((error) => {
+            console.error(error);
+        })
+
+}
+
+getlastCommitEvent('korgeron');
--- my-stuff/my-js/game.js
+++ my-stuff/my-js/game.js
@@ -1,10 +1,19 @@
-$('body').keydown(function (e){
-    console.log(e.key)
-    if (e.key === 'a'){
-        setInterval(function (){
-           $('#playerCharacter').
 
-        }, 0)
-    }
+let character = document.querySelector('#playerCharacter');
+let x = 100;
+let y = 100;
+let val = 10;
 
+let jump = character.style.top = `${y}px`;
+character.style.left = `${x}px`;
+
+console.log(jump);
+
+document.querySelector('body').addEventListener('keydown', (e) => {
+    console.log(e.code);
+   if (e.code === 'KeyA'){
+        --x
+   }
 })
+
+console.log(jump);
